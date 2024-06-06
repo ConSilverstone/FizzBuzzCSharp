1231cf9632e0a9364afe5eb6bfbc645d675a95ea
ConSilverstone <40634708+ConSilverstone@users.noreply.github.com>
2024-06-06 20:25:28 +0100

    Complete Program
    Was built in Sololearn so there was no commits for this project.
    Sololearn ID: ConSilverstone

---
+++ FizzBuzzC#/Program.cs
@@ -0,0 +1,34 @@
+﻿// Create our container and class //
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args) // Create a main function to hold the data
+        {
+            //Initilise Variables//
+            int fizz = 3; // For values that are multiple of this print fizz
+            int buzz = 5; // For values that are multiple of this print fizz
+            int i = 0; // We are going to use an i iternator, set to zero
+
+            for (i = 0; i <= 100; i++) // Increment i to 100 then stop
+            {
+                if (i % fizz == 0 && i % buzz != 0) // For values that are a multiple of fizz BUT NOT buzz
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % fizz != 0 && i % buzz == 0) // For values that are a multiple of buzz BUT NOT fizz
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if (i % fizz == 0 && i % buzz == 0) // For values that are a multiple of buzz AND fizz
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i); // Otherwise just print the number
+                }
+            }
+        }
+    }
+}
